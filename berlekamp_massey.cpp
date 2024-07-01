a8bc91c4decd13b24111aed0f107e67cf8df2c8c
Feiniu6ABC <41153766+Feiniu6ABC@users.noreply.github.com>
2024-07-01 13:53:51 +0800

    Update berlekamp_massey.cpp

--- berlekamp_massey.cpp
+++ berlekamp_massey.cpp
@@ -1,5 +1,3 @@
-//implement of berlekamp_massey algorithm in real number field
-
 #include <cstdio>
 #include <cmath>
 #include <vector>
@@ -7,103 +5,83 @@
 
 using namespace std;
 
-typedef double ld; 
-typedef vector<ld> vld; 
+typedef double ld;
+typedef vector<ld> vld;
 
 const int MAXN = 2333;
 const double EPS = 1e-7;
 
-int n, pn = 0, fail[MAXN];
-
 ld x[MAXN], delta[MAXN];
+int fail[MAXN]; // `fail` should be of type int
 
-vld prev_poly;
-vld best_poly;
-
-void printVector(const std::vector<double>& vec) {
-    for (const double& value : vec) {
-        std::cout << value << " ";
+void printVector(const vld& vec) {
+    for (const ld& value : vec) {
+        cout << value << " ";
     }
-    std::cout << std::endl; 
+    cout << endl;
 }
 
-
-int main() {
-    // input syndrome equation
-    //ld initial_values[] = {2, 4, 8, 16, 32, 64, 128, 256, 512, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+vld berlekampMassey(const ld* initial_values, int n) {
+    vld prev_poly, best_poly;
     
-    ld initial_values[] = {1,2,4,9,20,40,90};
-  
-    n = sizeof(initial_values) / sizeof(initial_values[0]);
-
     for (int i = 1; i <= n; i++) {
         x[i] = initial_values[i - 1];
     }
-
-    int best = 0; //index of the shortest r
+    
+    int best = 0, pn = 0;
+    
     for (int i = 1; i <= n; i++) {
-        // calculate discrepency
         ld dt = -x[i];
-
-        cout <<"value in current array : ";
-        for (int i=0; i<prev_poly.size(); i++){
-            cout <<prev_poly[i]<<" ";
-        }
-
         for (size_t j = 0; j < prev_poly.size(); j++) {
             dt += x[i - j - 1] * prev_poly[j];
         }
-        delta[i] = dt; // store discrepency of everystep
-
-        cout<<"fabs dt is "<<fabs(dt)<<endl;
-        if (fabs(dt) <= EPS) continue;                                           // next loop if close enough to 0
-
-        fail[pn] = i; 
+        delta[i] = dt;
+        
+        if (fabs(dt) <= EPS) continue;
+        
+        fail[pn] = i;
         if (!pn) {
-            
             pn++;
             prev_poly.resize(i);
-            //best_poly = prev_poly;
-            cout<<"pn is "<<pn<<endl;
             continue;
         }
-
+        
         vld &ls = best_poly;
-        ld k = -dt / delta[fail[best]]; 
-        cout <<"best is :";
-        printVector(best_poly);
-
-        cout<<"k is "<<k<<endl;
-
-        vld cur(i - fail[best] - 1, 0); 
-        cur.push_back(-k); 
-        cout<<"cur0 is: ";
-        printVector(cur);
+        ld k = -dt / delta[fail[best]]; // Correct the subscript
+        
+        vld cur(i - fail[best] - 1, 0);
+        cur.push_back(-k);
+        
         for (size_t j = 0; j < ls.size(); j++) {
-            cur.push_back(ls[j] * k);                                             //push back old poly*k
+            cur.push_back(ls[j] * k);
         }
-        cout<<"cur1 is: ";
-        printVector(cur);
+        
         if (cur.size() < prev_poly.size()) {
-            cur.resize(prev_poly.size(), 0);                                      
+            cur.resize(prev_poly.size(), 0);
         }
+        
         for (size_t j = 0; j < prev_poly.size(); j++) {
-            cur[j] += prev_poly[j];                                               
+            cur[j] += prev_poly[j];
         }
-        if (i - fail[best] + (int)best_poly.size() >= (int)prev_poly.size()) {
-            best = pn;                                                             //update best index
-            cout<<"best in is:"<<best<<endl;
+        
+        if (i - fail[best] + (int)ls.size() >= (int)prev_poly.size()) {
+            best = pn;
             best_poly = prev_poly;
         }
-        prev_poly = cur; 
+        
+        prev_poly = cur;
         ++pn;
     }
+    return prev_poly;
+}
 
-    for (size_t g = 0; g < prev_poly.size(); g++) {
-        cout << prev_poly[g] << " ";
-    }
-
-    cout << endl;
-
+int main() {
+    //ld initial_values[] = {1,2,4,9,20,40,90};
+    ld initial_values[] = {2, 4, 8, 16, 32, 64, 128, 256, 512, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+    int n = sizeof(initial_values) / sizeof(initial_values[0]);
+    
+    vld coeffs = berlekampMassey(initial_values, n);
+    printVector(coeffs);
+    
     return 0;
 }
