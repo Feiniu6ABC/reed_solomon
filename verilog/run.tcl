41515f33ccdc3301ebf41ac9813f339a2e0cb736
Feiniu6ABC <41153766+Feiniu6ABC@users.noreply.github.com>
2024-07-18 18:33:59 +0800

    Update run.tcl

--- verilog/run.tcl
+++ verilog/run.tcl
@@ -1,4 +1,5 @@
 ida_database -open -name="ida.db"
 ida_probe -log -sv_flow -uvm_reg -log_objects -sv_modules -wave -wave_probe_args= "BerlekampMassey_tb -depth all -all -memories -variables -packed 10000000 -unpacked 10000000 -dynamic" -wave_glitch_recording
+ida_probe -log -sv_flow -uvm_reg -log_objects -sv_modules -wave -wave_probe_args= "syndrome_tb -depth all -all -memories -variables -packed 10000000 -unpacked 10000000 -dynamic" -wave_glitch_recording
 run
 exit
