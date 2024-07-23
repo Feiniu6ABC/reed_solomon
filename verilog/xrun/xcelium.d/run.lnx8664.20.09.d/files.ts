72edef350c1418e444870eb010a1202895d11a08
autumn <autumn@localhost.localdomain>
2024-07-23 06:39:33 -0700

    fixed lut table input

--- verilog/xrun/xcelium.d/run.lnx8664.20.09.d/files.ts
+++ verilog/xrun/xcelium.d/run.lnx8664.20.09.d/files.ts
@@ -6,6 +6,6 @@
 1721446695 /home/autumn/Desktop/rs_code/reed_solomon/verilog/gf265_div.v
 1721446695 /home/autumn/Desktop/rs_code/reed_solomon/verilog/tree_adder.v
 1721447130 /home/autumn/Desktop/rs_code/reed_solomon/verilog/gf256_power_lut.v
-1721458000 /home/autumn/Desktop/rs_code/reed_solomon/verilog/syndrome_slice.v
+1721741842 /home/autumn/Desktop/rs_code/reed_solomon/verilog/syndrome_slice.v
 1721446695 /home/autumn/Desktop/rs_code/reed_solomon/verilog/syndrome.v
-1721449699 /home/autumn/Desktop/rs_code/reed_solomon/verilog/syndrome_test.sv
+1721739129 /home/autumn/Desktop/rs_code/reed_solomon/verilog/syndrome_test.sv
